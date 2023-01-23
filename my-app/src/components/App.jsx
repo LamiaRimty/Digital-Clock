1cba26755228d6f42b974c4b74c1cb2218621dbb
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-01-23 12:48:38 +0100

    Show current time

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
 
 
 function App() {
+  const time = new Date().toLocaleTimeString();
+  const [currentTime,setCurrentTime]=useState(time);
+ 
   return (
     <div>
-      <h1 className="heading">Digital Clock ⏰</h1>
+      <h1>Digital Clock ⏰</h1>
        <div className="container">
-            <h1>Time</h1>
+            <h1 className="heading">{currentTime}</h1>
+            <button onClick={updateTime}>Get Time</button>
        </div>
+       <footer>Made with 🌷 in Delft</footer>
     </div>
   );
 }
